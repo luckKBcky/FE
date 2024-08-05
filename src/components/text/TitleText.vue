83ce260b40b0af0f9cdcc597af609e26023b8b26
Mirae Kwak <alfo9490@naver.com>
2024-08-05 14:48:48 +0900

    :lipstick: 소비내역 분석 화면 UI 구현

---
+++ src/components/text/TitleText.vue
@@ -0,0 +1,27 @@
+<!-- 
+** TitleText 사용법 **
+
+ <Cardhead 
+ titleText = "제목"/>
+
+-->
+<script setup>
+const props = defineProps({
+  titleText: {
+    type: String,
+    default: "제목",
+  }
+});
+</script>
+
+<template>
+    <h1>{{ titleText }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 600;
+  color: #464255;
+  font-size: 14px;
+}
+</style>
